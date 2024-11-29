e04a23405848e92ebda3869309dfd10eee376074
Gustavo Souza <gustavosouza1688@gmail.com>
2024-11-29 13:42:03 -0300

    feat: add product entity

---
+++ Entities/Product.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace burger_mania_api.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int Id {get; set;} 
+
+        [StringLength(127)]
+        [Required]
+        public required string Name {get; set;}
+
+        [Required]
+        [StringLength(255)]
+        public required string ImagePath {get; set;}
+
+        [Required]
+        public required float Price {get; set;}
+
+        [StringLength(255)]
+        [Required]
+        public required string Ingredients {get; set;}
+
+        [StringLength(511)]
+        [Required]
+        public required string Description {get; set;}
+
+        public int? CategoryId { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]    
+        [JsonIgnore]
+        public Category? Category { get; set; } 
+    }   
+}
