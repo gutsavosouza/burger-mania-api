ae57569c4bfa00d14a9480025c0dbd70d8f02b8a
Gustavo Souza <gustavosouza1688@gmail.com>
2024-11-29 13:42:24 -0300

    feat: add category dto class

---
+++ DTOs/CategoryDTO.cs
@@ -0,0 +1,9 @@
+namespace burger_mania_api.DTOs
+{
+    public class CategoryDTO
+    {
+        public required string Name {get; set;}
+        public required string Description {get; set;}
+        public required string ImagePath {get; set;}
+    }
+}
