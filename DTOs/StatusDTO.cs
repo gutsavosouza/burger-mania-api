299f139331891155e6fc21b06665bdbcaca17fe4
Gustavo Souza <gustavosouza1688@gmail.com>
2024-12-04 15:06:58 -0300

    refactor: make the name field required in the dto

--- DTOs/StatusDTO.cs
+++ DTOs/StatusDTO.cs
@@ -2,7 +2,7 @@ namespace burger_mania_api.DTOs
 {
     public class StatusDTO
     {
-        public string Name { get; set; } = string.Empty;
+        public required string Name { get; set; } = string.Empty;
     }
 
 }
