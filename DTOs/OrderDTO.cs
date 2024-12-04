fd2dbe0e52090b41f78ed9099204096052248cc8
Gustavo Souza <gustavosouza1688@gmail.com>
2024-12-04 15:06:13 -0300

    refactor: removed the total price from the dto

--- DTOs/OrderDTO.cs
+++ DTOs/OrderDTO.cs
@@ -2,7 +2,6 @@ namespace burger_mania_api.DTOs
 {
     public class OrderDTO
     {
-        public int StatusId { get; set; }
-        public float TotalPrice { get; set; }
+        public required int StatusId { get; set; }
     }
 }
